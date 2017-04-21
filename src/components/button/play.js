c3f22556fab8397dbae71a3789886c6e797dc5b5
Renan Liberato <renan.libsantana@gmail.com>
2017-04-21 13:32:39 -0300

    SVG components implementation to use on app actions.
    Each button has a default prop 'onTouchTap', that is bound to the onTouchTap event of IconButton component.

---
+++ src/components/button/play.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import IconButton from 'material-ui/IconButton'
+
+export default class Play extends Component {
+    render() {
+        if (this.props.passing) {
+            return (
+                <IconButton
+                    tooltip="Pause"
+                    touch={true}
+                    tooltipPosition="top-left"
+                    onTouchTap={this.props.onTouchTap}>
+                    <svg fill="#000000" height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                    </svg>
+                </IconButton>
+            )
+        }
+        return (
+            <IconButton
+                tooltip="Play"
+                touch={true}
+                tooltipPosition="top-left"
+                onTouchTap={this.props.onTouchTap}>
+                <svg fill="#000000" height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 5v14l11-7z"/>
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                </svg>
+            </IconButton>
+        )
+    }
+}
